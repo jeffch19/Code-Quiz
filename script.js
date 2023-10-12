3e830f8deea8ed42d881aaf5bc962d35b90882ea
jeffch19 <17jcummings@gmail.com>
2023-10-12 14:48:16 -0400

    adding all of the questions as well as answer id's

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Makes the questions disappear
+
+// const questionContainers = document.querySelectorAll('.questionContainer');
+
+// questionContainers.forEach(function(questionContainer) {
+//   questionContainer.addEventListener('click', function() {
+//     questionContainer.style.display = 'none';
+//   });
+// });
