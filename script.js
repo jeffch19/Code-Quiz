3d7d74b908e1b5f2719c4ab81909bc3f91bedbc3
jeffch19 <17jcummings@gmail.com>
2023-10-13 16:03:39 -0400

    making javascript load once the page loads and playing around more with the timer + trying to get the start quiz to work

--- script.js
+++ script.js
@@ -1,46 +1,107 @@
-// Timer Starts when start quiz is clicked
-let timeLeft = 75;
-const timePenalty = 10;
-let timerInterval; 
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize variables
+  let timeLeft = 75;
+  const timePenalty = 10;
+  let timerInterval;
+  let currentQuestionIndex = 0;
+  let correctAnswers = 0;
+  let highScore = 0;
 
-function endQuiz() {
-  clearInterval(timerInterval);
-  console.log("Quiz has ended.");
-  
-}
+  // Define your questions
+  const questions = [
+    {
+      text: "Commonly Used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      correct: 2,
+    },
+    {
+      text: "The condition in an if / else statement is enclosed with __________.",
+      choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+      correct: 2,
+    },
+    {
+      text: "Arrays in JavaScript can be used to store __________.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all the above"],
+      correct: 3,
+    },
+    {
+      text: "String values must be enclosed within __________ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+      correct: 2,
+    },
+    {
+      text: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+      correct: 3,
+    },
+    {
+      text: "What is the correct way to declare a variable in JavaScript?",
+      choices: ["var myVariable", "let myVariable", "const myVariable", "All of the above"],
+      correct: 3,
+    },
+    // Add more questions here
+  ];
 
-function updateTimer() {
-  document.getElementById("timer").textContent = timeLeft;
+  // Function to display the current question
+  function displayQuestion(index) {
+    const questionContainer = document.querySelector('.question-container');
+    const questionText = document.querySelector('.question-text');
+    const choices = document.querySelector('.choices');
 
-  if (timeLeft <= 0) {
-    endQuiz();
-  } else {
-    timeLeft--;
+    if (index < questions.length) {
+      questionText.textContent = questions[index].text;
+      choices.innerHTML = ""; // Clear previous choices
+
+      questions[index].choices.forEach((choice, choiceIndex) => {
+        const button = document.createElement("button");
+        button.textContent = `${choiceIndex + 1}. ${choice}`;
+        choices.appendChild(button);
+        button.addEventListener("click", function () {
+          if (choiceIndex === questions[index].correct) {
+            correctAnswers++;
+          } else {
+            timeLeft -= timePenalty;
+          }
+
+          currentQuestionIndex++;
+          displayQuestion(currentQuestionIndex);
+        });
+      });
+    } else {
+      endQuiz();
+    }
   }
-}
 
-function handleWrongAnswer() {
-  timeLeft -= timePenalty;
-  if (timeLeft <= 0) {
-    endQuiz();
+  function updateTimer() {
+    document.getElementById("timer").textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      endQuiz();
+    } else {
+      timeLeft--;
+    }
   }
-}
 
-document.getElementById("start-btn").addEventListener("click", function () {
-  timerInterval = setInterval(updateTimer, 1000); 
-  handleWrongAnswer();
+  function endQuiz() {
+    clearInterval(timerInterval);
+    highScore = Math.max(highScore, correctAnswers);
+    console.log("Quiz has ended.");
+    console.log("Correct Answers: " + correctAnswers);
+    console.log("High Score: " + highScore);
+    // You can save the high score to local storage here
+  }
+
+  // Start the quiz when the "Start Quiz" button is clicked
+  document.getElementById("start-btn").addEventListener("click", function () {
+    timerInterval = setInterval(updateTimer, 1000);
+    displayQuestion(currentQuestionIndex);
+  });
 });
 
 
 
 
 
-
-
-
-
-// Make an array of the questions
-var questions = document.getElementsByClassName("question-container")
 
 // Makes the questions disappear
 
