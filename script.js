39086c9b65c3233fb20c3766f3e2e9e0660815a0
jeffch19 <17jcummings@gmail.com>
2023-10-13 14:48:49 -0400

    added timer functions

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 // Timer Starts when start quiz is clicked
 let timeLeft = 75;
+const timePenalty = 10;
+let timerInterval; 
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  console.log("Quiz has ended.");
+  
+}
 
 function updateTimer() {
   document.getElementById("timer").textContent = timeLeft;
+
+  if (timeLeft <= 0) {
+    endQuiz();
+  } else {
+    timeLeft--;
+  }
 }
 
+function handleWrongAnswer() {
+  timeLeft -= timePenalty;
+  if (timeLeft <= 0) {
+    endQuiz();
+  }
+}
 
+document.getElementById("start-btn").addEventListener("click", function () {
+  timerInterval = setInterval(updateTimer, 1000); 
+  handleWrongAnswer();
+});
 
 
 
